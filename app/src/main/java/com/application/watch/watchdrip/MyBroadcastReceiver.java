080ed54584c2e7b069d0943cc639b209c70e0161
miguelavh <miguelavh100@gmail.com>
2024-11-01 19:21:43 +0100

    code update

--- app/src/main/java/com/application/watch/watchdrip/MyBroadcastReceiver.java
+++ app/src/main/java/com/application/watch/watchdrip/MyBroadcastReceiver.java
@@ -5,11 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 
-/*import androidx.work.OneTimeWorkRequest;
-import androidx.work.WorkRequest;
-import androidx.work.Worker;
-import androidx.work.WorkerParameters;*/
-
 public class MyBroadcastReceiver extends BroadcastReceiver {
 
     @Override
@@ -18,18 +13,11 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
         // Create Intent
         Intent serviceIntent = new Intent(context, MyService.class);
         // Start service
-        /*if(Build.VERSION.SDK_INT >= 34)
-        {
-            WorkRequest uploadWorkRequest = new OneTimeWorkRequest.Builder(UploadWorker.class).build();
-            androidx.work.WorkManager.getInstance(context).enqueue(uploadWorkRequest);
-        }
-        else*/ if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+	if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             context.startForegroundService(serviceIntent);
         } else {
             context.startService(serviceIntent);
         }
-        //context.startService(serviceIntent);
-
     }
 }
 
